b3709d17477a8cc5c458ef3ba8dc3fff92b5198b
pauljan <zhangjun@56sino.cn>
2018-09-04 16:38:03 +0800

    ss

--- suanpi/views.py
+++ suanpi/views.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from suanpi.models import *
 from django.template.context_processors import request
 # Create your views here.
+
+def global_setting(request):
+    web_url = 'http://www.suanpi.com' 
+
 def fee_index(request):
     fee_list = Tblfee.objects.all()
     return render(request,'index.html',{'fee_list':fee_list})
